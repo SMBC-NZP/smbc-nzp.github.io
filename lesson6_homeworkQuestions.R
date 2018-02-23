bca650bdb1a38891444c263efa77a3acfb4229af
bsevansunc <bsevans.unc@gmail.com>
2018-02-23 10:31:57 -0500

    initial commit

---
+++ lesson6_homeworkQuestions.R
@@ -0,0 +1,89 @@
+#=================================================================================*
+# ---- setup ----
+#=================================================================================*
+
+# Load libraries:
+
+library(RCurl)
+library(lubridate)
+
+# Load a source script:
+
+script <-
+  getURL(
+    "https://raw.githubusercontent.com/bsevansunc/workshop_languageOfR/master/sourceCode.R"
+  )
+
+# Evaluate then remove the source script:
+
+eval(parse(text = script))
+
+rm(script)
+
+library(lubridate)
+
+options(knitr.table.format = "html")
+
+#=================================================================================*
+# 1. The population of the Bahamas ----
+#=================================================================================*
+
+whoPopulation
+
+# Calculate the population of the Bahamas from 1995-2013. Return your results as 
+# a two-column data frame with the columns year and population.
+
+
+
+#=================================================================================*
+# 2. The population density of US regions ----
+#=================================================================================*
+
+states1975
+
+# Use group_by and summarize to calculate the population density of each region.
+# Return your results as a two-column data frame with the columns region and 
+# popDensity.   
+
+
+
+#=================================================================================*
+# 3. Counting birds at site "apple" ----
+#=================================================================================*
+
+birdCounts
+
+birdHabits
+
+# Calculate the total number of each diet class observed at site "apple" in  2009.
+# Return your results as a two-column data frame with the columns diet and count.
+
+
+
+#=================================================================================*
+# 4. Star Wars ----
+#=================================================================================*
+
+measurements
+
+origins
+
+# Calculate the average mass and number of characters associated with each  
+# species of the Star Wars universe. Return your results as a three-column data
+# frame with the columns: species, nCharacters, and meanMass.
+
+
+
+#=================================================================================*
+# 5. Iris petal shape ----
+#=================================================================================*
+
+irisTbl
+
+# You have developed a derived variable, "petalShape" to describe Iris flowers. 
+# This variable is defined as the ratio of petalLength to petalWidth 
+# (petalLength/petalWidth). Calculate the average (mean) shape of petals in the
+# iris dataset. Return your results as a two-column data frame with the columns 
+# species and petalShape_mean.
+
+
